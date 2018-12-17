3943f2599a3dda8d8edb5a08fdee0a5e41deeb3d
Jad Mintun <jadmintun@gmail.com>
2018-12-17 15:56:50 -0500

    0.1.12

--- Source/scripts/model.js
+++ Source/scripts/model.js
@@ -3,12 +3,12 @@
     let currVideoNum = null;
 
     const videosDataArray = [
-        { src: "1CnMUaokoSo", thumbSrc: "assets/video6-thumb.jpg", title: "Old Friends Senior Dog Sanctuary", link: "http://ofsds.org/index.html" },
-        { src: "tNMfBs6kKK0", thumbSrc: "assets/video00-thumb.jpg", title: "Bergin University of Canine Studies", link: "https://www.berginu.edu/" }, // warrior canine connection - nursery cam - http://explore.org/live-cams/player/service-puppy-cam
-        { src: "wi8rq-t_PBc", thumbSrc: "assets/video0-thumb.jpg", title: "Great Danes Nursery" }, // warrior canine connection - nursery cam - http://explore.org/live-cams/player/service-puppy-cam
-        { src: "RxuR0qFD6yo", thumbSrc: "assets/video1-thumb.jpg", title: "Warrior Canine Connection Nursery Cam", link: "https://warriorcanineconnection.org/" }, // warrior canine connection - nursery cam - http://explore.org/live-cams/player/service-puppy-cam-3
-        { src: "PPmnFbpnvMQ", thumbSrc: "assets/video2-thumb.jpg", title: "Golden Retriever Puppies - Daisy's Litter at ECAD", link: "https://www.ecad1.org/" }, // Golden Retriever Puppies - Daisy’s Litter at ECAD - http://explore.org/live-cams/player/east-coast-assistance-dogs-cam-2
-        { src: "RTfKYDIhlVE", thumbSrc: "assets/video3-thumb.jpg", title: "Golden Retriever Puppies - Midori's Litter at ECAD", link: "https://www.ecad1.org/" }, // ECAD - Midori's Litter - http://explore.org/live-cams/player/east-coast-assistance-dogs-cam
+        { src: "3xQRVD4JdoE", thumbSrc: "assets/video6-thumb.jpg", title: "Old Friends Senior Dog Sanctuary", link: "http://ofsds.org/index.html" },
+        { src: "r3qKh8VLcZM", thumbSrc: "assets/video7-thumb.jpg", title: "Guiding Eyes for the Blind Guide Dog Puppies", link: "https://www.guidingeyes.org/givingtuesday/" }, 
+        { src: "DzNqu7_pevQ", thumbSrc: "assets/video0-thumb.jpg", title: "Great Danes Nursery" }, // warrior canine connection - nursery cam - http://explore.org/live-cams/player/service-puppy-cam
+        { src: "wJ5M8dTpMaI", thumbSrc: "assets/video1-thumb.jpg", title: "Warrior Canine Connection Nursery Cam", link: "https://warriorcanineconnection.org/" }, // warrior canine connection - nursery cam - http://explore.org/live-cams/player/service-puppy-cam-3
+        { src: "AK0_-qGixYE", thumbSrc: "assets/video2-thumb.jpg", title: "Golden Retriever Puppies - Daisy's Litter at ECAD", link: "https://www.ecad1.org/" }, // Golden Retriever Puppies - Daisy’s Litter at ECAD - http://explore.org/live-cams/player/east-coast-assistance-dogs-cam-2
+        // { src: "RTfKYDIhlVE", thumbSrc: "assets/video3-thumb.jpg", title: "Golden Retriever Puppies - Midori's Litter at ECAD", link: "https://www.ecad1.org/" }, // ECAD - Midori's Litter - http://explore.org/live-cams/player/east-coast-assistance-dogs-cam
     ];
     const VIDEO_CHANGED = "VIDEO_CHANGED",
         APP_READY = "APP_READY";
